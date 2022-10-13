6e39d2b60d2cc407f47c2d6fe692d60bd9e156ea
enmeraz <eduardo.meraz12@gmail.com>
2022-10-12 23:41:29 -0600

    added db code

--- App/lib/model/stars.dart
+++ App/lib/model/stars.dart
@@ -2,6 +2,10 @@
 final String tableStars = 'stars';
 
 class StarFields {
+  static final List<String> values = [
+    id, name, description   /* add all fields */
+  ];
+  //representing all of our fields of our model objects
   static final String id = '_id';
   static final String name = 'name';
   static final String description = 'description';
@@ -17,4 +21,29 @@ class Stars{
     required this.name,
     required this.description
   });
+
+  //insert 
+  Stars copy({
+    int? id,
+    String? name,
+    String? description
+  }) => 
+      //copy of our current node object
+      Stars(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        description: description ?? this.description
+      );
+
+  static Stars fromJson(Map<String,Object?> json) => Stars(
+      id: json[StarFields.id] as int?,
+      name: json[StarFields.name] as String,
+      description: json[StarFields.description] as String
+      );
+
+  Map<String, Object?> toJson() => {
+    StarFields.id : id,
+    StarFields.name : name, 
+    StarFields.description : description
+  };
 }
