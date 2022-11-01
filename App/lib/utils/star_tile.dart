498134d61e8698147856b598b3e3c2531e972dd5
axelgarces69 <77650295+axelgarces69@users.noreply.github.com>
2022-11-01 09:22:32 -0600

    Add files via upload

---
+++ App/lib/utils/star_tile.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class StarTile extends StatelessWidget {
+  const StarTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 25.0, bottom: 25),
+      child: Container(
+        padding: EdgeInsets.all(12),
+        width: 250,
+        decoration: BoxDecoration(
+            color: Colors.deepOrange[800],
+            borderRadius: BorderRadius.circular(12)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset('lib/images/image1.jpg'),
+            ),
+            Text(
+              'Step 1.',
+              style: GoogleFonts.bebasNeue(fontSize: 25),
+            ),
+            Text(
+              'Safety is the most important. Please make sure you are in a safe location.\n\nWear Protective eye wear and dont look at lazer derictly!',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ App/lib/utils/star_tile2.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class StarTile2 extends StatelessWidget {
+  const StarTile2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 25.0, bottom: 25),
+      child: Container(
+        padding: EdgeInsets.all(12),
+        width: 250,
+        decoration: BoxDecoration(
+            color: Colors.deepOrange[800],
+            borderRadius: BorderRadius.circular(12)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset('lib/images/image3.jpg'),
+            ),
+            Text(
+              'Step 3.',
+              style: GoogleFonts.bebasNeue(fontSize: 25),
+            ),
+            Text(
+              "Safety is the most important. Please make sure you are in a safe location.\n\nWear Protective eye wear and dont look at lazer derictly!",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
