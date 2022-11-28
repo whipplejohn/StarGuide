67b3dc9a5f36c45e40d3de98a36bd51733b8410e
axelgarces69 <77650295+axelgarces69@users.noreply.github.com>
2022-11-28 15:50:42 -0700

    Update star_tile.dart

--- App/lib/utils/star_tile.dart
+++ App/lib/utils/star_tile.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 class StarTile extends StatelessWidget {
   const StarTile({super.key});
@@ -9,10 +12,10 @@ class StarTile extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(left: 25.0, bottom: 25),
       child: Container(
-        padding: EdgeInsets.all(12),
+        padding: const EdgeInsets.all(12),
         width: 250,
         decoration: BoxDecoration(
-            color: Colors.deepOrange[800],
+            color: const Color.fromARGB(255, 44, 62, 251),
             borderRadius: BorderRadius.circular(12)),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -22,11 +25,12 @@ class StarTile extends StatelessWidget {
               child: Image.asset('lib/images/image1.jpg'),
             ),
             Text(
-              'Step 1.',
+              'step1_card_title'.tr(),
               style: GoogleFonts.bebasNeue(fontSize: 25),
             ),
             Text(
-              'Safety is the most important. Please make sure you are in a safe location.\n\nWear Protective eye wear and dont look at lazer derictly!',
+              'step1_card_body'.tr(),
+              style: GoogleFonts.bebasNeue(fontSize: 10),
             ),
           ],
         ),
