a173f9d7c071e51aab16f3915bac3d1db9baea99
axelgarces69 <77650295+axelgarces69@users.noreply.github.com>
2022-12-05 16:11:41 -0700

    Update star_tile1.dart

--- App/lib/utils/star_tile1.dart
+++ App/lib/utils/star_tile1.dart
@@ -1,7 +1,19 @@
 // ignore_for_file: import_of_legacy_library_into_null_safe
 
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+_launchRadarLink() async {
+  var url = Uri.parse("https://www.flightradar24.com/");
+  if (await canLaunchUrl(url)) {
+    await launchUrl(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
 
 class StarTile1 extends StatelessWidget {
   const StarTile1({super.key});
@@ -9,7 +21,7 @@ class StarTile1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 25.0, bottom: 25),
+      padding: const EdgeInsets.only(left: 25.0, bottom: 25, right: 25),
       child: Container(
         padding: const EdgeInsets.all(12),
         width: 250,
@@ -23,13 +35,26 @@ class StarTile1 extends StatelessWidget {
               borderRadius: BorderRadius.circular(12),
               child: Image.asset('lib/images/image2.jpg'),
             ),
-            Text(
-              'Step 2.',
-              style: GoogleFonts.bebasNeue(fontSize: 25),
+            SizedBox(
+              height: 10,
             ),
             Text(
-                'Safety is the most important. Please make sure you are in a safe location.\n\nWear Protective eye wear and dont look at lazer derictly!',
-                style: GoogleFonts.bebasNeue(fontSize: 10)),
+              'step2_card_title'.tr(),
+              style: GoogleFonts.bebasNeue(fontSize: 25),
+            ),
+            Text('step2_card_body'.tr(),
+                style: GoogleFonts.bebasNeue(fontSize: 22)),
+            ElevatedButton(
+              onPressed: _launchRadarLink,
+              child: Text("Flight Radar"),
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                backgroundColor: Colors.red,
+                shadowColor: Colors.white,
+                side: BorderSide(color: Colors.white, width: .7),
+                padding: EdgeInsets.symmetric(horizontal: 148, vertical: 25),
+              ),
+            ),
           ],
         ),
       ),
